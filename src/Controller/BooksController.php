56eb0aa4c85822eb86da915cc4a877609ec64f59
OLEG MANDRYCHENKO <mandrik89@gmail.com>
2020-05-06 09:14:47 +0200

    add book

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -5,14 +5,43 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Form\BookType;
 use App\Repository\BookRepository;
+use Cocur\Slugify\Slugify;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class BooksController extends AbstractController
 {
     /** @var BookRepository $bookRepository */
     private $bookRepository;
+
+    /**
+     * @Route("/books/add", name="book_add")
+     */
+    public function addBook(Request $request, Slugify $slugify)
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book->setSlug($slugify->slugify($book->getTitle()));
+            $book->setCreatedAt(new \DateTime());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            return $this->redirectToRoute('book_add');
+        }
+        return $this->render('books/add.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 
     public function __construct(BookRepository $bookRepository)
     {
@@ -40,5 +69,6 @@ class BooksController extends AbstractController
             'book' => $book
         ]);
     }
+    
     
 }
--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -4,19 +4,20 @@ namespace App\Form;
 
 use App\Entity\Book;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class BookType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('author_id')
-            ->add('title')
-            ->add('description')
-            ->add('slug')
-            ->add('created_at')
+            ->add('author_id', IntegerType::class, ['label' => ''])
+            ->add('title', TextType::class, ['label' => ''])
+            ->add('description', TextareaType::class, ['label' => ''])
         ;
     }
 
