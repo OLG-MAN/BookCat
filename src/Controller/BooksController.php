48f257b8543e9c98f7224f116d4af6ca6d13a693
OLEG MANDRYCHENKO <mandrik89@gmail.com>
2020-05-06 11:18:13 +0200

    books crud

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -8,16 +8,20 @@ use App\Entity\Book;
 use App\Form\BookType;
 use App\Repository\BookRepository;
 use Cocur\Slugify\Slugify;
-use Doctrine\ORM\EntityManagerInterface;
+// use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 class BooksController extends AbstractController
 {
     /** @var BookRepository $bookRepository */
     private $bookRepository;
+
+    public function __construct(BookRepository $bookRepository)
+    {
+        $this->bookRepository = $bookRepository;
+    }
 
     /**
      * @Route("/books/add", name="book_add")
@@ -36,16 +40,56 @@ class BooksController extends AbstractController
             $em->persist($book);
             $em->flush();
 
-            return $this->redirectToRoute('book_add');
+            return $this->redirectToRoute('books');
         }
         return $this->render('books/add.html.twig', [
             'form' => $form->createView()
         ]);
     }
 
-    public function __construct(BookRepository $bookRepository)
+    /**
+     * @Route("/books/{slug}/edit", name="book_edit")
+     */
+    public function edit(Book $book, Request $request, Slugify $slugify)
     {
-        $this->bookRepository = $bookRepository;
+         $form = $this->createForm(BookType::class, $book);
+         $form->handleRequest($request);
+
+         if ($form->isSubmitted() && $form->isValid()) {
+            $book->setSlug($slugify->slugify($book->getTitle()));
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('book_show', [
+                'slug' => $book->getSlug()
+            ]);
+         }
+
+         return $this->render('books/add.html.twig', [
+             'form' => $form->createView()
+         ]);
+    }
+
+    /**
+     * @Route("/books/{slug}/delete", name="book_delete")
+     */
+    public function delete(Book $book)
+    {     
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($book);
+        $em->flush();
+
+        return $this->redirectToRoute('books');
+    }
+
+    /**
+     * @Route("/books/{slug}", name="book_show")
+     */
+    public function book(Book $book)
+    {
+        return $this->render('books/show.html.twig', [
+            'book' => $book
+        ]);
     }
 
     /**
@@ -58,17 +102,5 @@ class BooksController extends AbstractController
         return $this->render('books/index.html.twig', [
            'books' => $books
         ]);
-    }
-
-    /**
-     * @Route("/books/{slug}", name="book_show")
-     */
-    public function book(Book $book)
-    {
-        return $this->render('books/show.html.twig', [
-            'book' => $book
-        ]);
-    }
-    
-    
+    }    
 }
